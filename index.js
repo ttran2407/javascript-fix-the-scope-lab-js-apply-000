958a11a7c7d440d08e5042c781fd7c44484107e5
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 16:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  // the var two need to be defined before using it
   const two = 2
-  return n + two
 
-  // Feel free to move things around!
+  return n + two
 
 }
 
@@ -26,4 +26,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
